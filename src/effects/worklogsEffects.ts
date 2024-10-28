8bf46800d5de79887082515edd7a2aa54cd5cb4a
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2024-10-28 17:54:17 +0600

    Update

--- src/effects/worklogsEffects.ts
+++ src/effects/worklogsEffects.ts
@@ -7,6 +7,7 @@ import {
   setWorklogs,
   TPerformetOption,
 } from 'slices/worklogs';
+import CalculateHoursFromTrackerTask from 'utils/calculateHoursFromTrackerTack';
 
 // 2024-10-20T16:07:17+03:00 - full valid date
 export const FORMAT_TYPE = 'YYYY-MM-DD';
@@ -18,75 +19,50 @@ export const getWorklogs = (
 ) => {
   return async function (dispatch: Dispatch<any>) {
     // reset state before fetching new data
-    dispatch(setLoading(true));
     dispatch(setError(''));
+    dispatch(setLoading(true));
     dispatch(setWorklogs(null));
 
     // fix date format to API request
     const _dateFrom = `${dayjs(dateFrom).format(FORMAT_TYPE)}T00:00:00`;
     const _dateTo = `${dayjs(dateTo).format(FORMAT_TYPE)}T23:59:59`;
 
-    const { data, success, error } = await WorklogService.searchWorklogs(
-      `${selectedPerformers[0].key}`,
-      _dateFrom,
-      _dateTo,
-    );
+    try {
+      const { data, success } = await WorklogService.searchWorklogs(
+        `${selectedPerformers[0].key}`,
+        _dateFrom,
+        _dateTo,
+      );
 
-    // success
-    if (success && data) {
-      dispatch(setLoading(false));
-      dispatch(setWorklogs(data));
+      if (success && data) {
+        dispatch(setLoading(false));
 
-      // error
-    } else {
+        const newData = data.reduce((total: any, item: any) => {
+          // обрезаем строку даты до формата YYYY-MM-DD
+          const logDay = item.createdAt.slice(0, 10);
+
+          // создаем объект задач с нужными нам полями
+          const logTask = {
+            code: item.issue.key,
+            name: item.issue.display,
+            link: item.issue.self,
+            comment: item.comment || null,
+            createdAt: item.createdAt,
+            duration: CalculateHoursFromTrackerTask(item.duration),
+          };
+
+          // добавляем день как поле объекта со значениям массива залогированных задач
+          total[logDay] = [...(total[logDay] || []), logTask];
+
+          return total;
+        }, {});
+
+        dispatch(setWorklogs(newData));
+      }
+    } catch (e) {
       dispatch(setLoading(false));
-      dispatch(setError(error));
+      dispatch(setWorklogs(e));
+      dispatch(setError(`${e} - Error fetching worklogs`));
     }
   };
 };
-
-// getWorkLog
-// const getWorkLog = async () => {
-//   dispatch(setLoading(true));
-//   dispatch(setWorklogs(null));
-
-//   try {
-//     const { data } = await httpClient.post('/worklog/_search', {
-//       createdBy: selectedPerformerIds[0],
-//       createdAt: {
-//         from: `${dayjs(dateFrom).format(FORMAT_TYPE)}T00:00:00`,
-//         to: `${dayjs(dateTo).format(FORMAT_TYPE)}T23:59:59`,
-//       },
-//     });
-
-//     if (data) {
-//       dispatch(setLoading(false));
-//       console.log('data', data);
-
-//       const newData = data.reduce((total: any, item: any) => {
-//         // обрезаем строку даты до формата YYYY-MM-DD
-//         const logDay = item.createdAt.slice(0, 10);
-
-//         // создаем объект задач с нужными нам полями
-//         const logTask = {
-//           code: item.issue.key,
-//           name: item.issue.display,
-//           link: item.issue.self,
-//           comment: item.comment || null,
-//           createdAt: item.createdAt,
-//           duration: CalculateHoursFromTrackerTask(item.duration),
-//         };
-
-//         // добавляем день как поле объекта со значениям массива залогированных задач
-//         total[logDay] = [...(total[logDay] || []), logTask];
-
-//         return total;
-//       }, {});
-
-//       dispatch(setWorklogs(newData));
-//     }
-//   } catch (e) {
-//     dispatch(setLoading(false));
-//     dispatch(setWorklogs(e));
-//   }
-// };
